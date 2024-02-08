0cd227de4bf538a5bd256bf33b77878a0da0dc0f
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-02-08 21:28:37 +0300

    style(plugins): apply formatting

--- lua/user/plugins/nav/telescope.lua
+++ lua/user/plugins/nav/telescope.lua
@@ -63,10 +63,10 @@ local function telescope_options()
                 themes.get_dropdown({ previewer = false }),
             },
             ["fzf"] = {
-                fuzzy = true,                   -- false will only do exact matching
+                fuzzy = true, -- false will only do exact matching
                 override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true,    -- override the file sorter
-                case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             },
         },
     }
