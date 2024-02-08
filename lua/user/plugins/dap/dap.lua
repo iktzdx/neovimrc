2731b987f65d8bd43a330d737fedc9ce9f0806dd
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-02-08 22:54:20 +0300

    style(dap): apply formatting

--- lua/user/plugins/dap/dap.lua
+++ lua/user/plugins/dap/dap.lua
@@ -21,51 +21,36 @@ local function dap_config()
     )
 
     -- define dap signs
-    vim.fn.sign_define(
-        "DapBreakpoint",
-        {
-            text = "",
-            texthl = "DapBreakpoint",
-            linehl = "DapBreakpoint",
-            numhl = "DapBreakpoint",
-        }
-    )
-    vim.fn.sign_define(
-        "DapBreakpointCondition",
-        {
-            text = "ﳁ",
-            texthl = "DapBreakpoint",
-            linehl = "DapBreakpoint",
-            numhl = "DapBreakpoint",
-        }
-    )
-    vim.fn.sign_define(
-        "DapBreakpointRejected",
-        {
-            text = "",
-            texthl = "DapBreakpoint",
-            linehl = "DapBreakpoint",
-            numhl = "DapBreakpoint",
-        }
-    )
-    vim.fn.sign_define(
-        "DapLogPoint",
-        {
-            text = "",
-            texthl = "DapLogPoint",
-            linehl = "DapLogPoint",
-            numhl = "DapLogPoint",
-        }
-    )
-    vim.fn.sign_define(
-        "DapStopped",
-        {
-            text = "",
-            texthl = "DapStopped",
-            linehl = "DapStopped",
-            numhl = "DapStopped",
-        }
-    )
+    vim.fn.sign_define("DapBreakpoint", {
+        text = "",
+        texthl = "DapBreakpoint",
+        linehl = "DapBreakpoint",
+        numhl = "DapBreakpoint",
+    })
+    vim.fn.sign_define("DapBreakpointCondition", {
+        text = "ﳁ",
+        texthl = "DapBreakpoint",
+        linehl = "DapBreakpoint",
+        numhl = "DapBreakpoint",
+    })
+    vim.fn.sign_define("DapBreakpointRejected", {
+        text = "",
+        texthl = "DapBreakpoint",
+        linehl = "DapBreakpoint",
+        numhl = "DapBreakpoint",
+    })
+    vim.fn.sign_define("DapLogPoint", {
+        text = "",
+        texthl = "DapLogPoint",
+        linehl = "DapLogPoint",
+        numhl = "DapLogPoint",
+    })
+    vim.fn.sign_define("DapStopped", {
+        text = "",
+        texthl = "DapStopped",
+        linehl = "DapStopped",
+        numhl = "DapStopped",
+    })
 
     -- keymaps
     vim.keymap.set("n", "<leader>bb", function()
