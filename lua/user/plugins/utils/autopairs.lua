56fd70e9ab94e55fba344f303777c5ee62939ff5
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-02-08 21:35:57 +0300

    feat(autopairs): enable plugin

--- lua/user/plugins/utils/autopairs.lua
+++ lua/user/plugins/utils/autopairs.lua
@@ -18,7 +18,8 @@ local function autopairs_config(_, opts)
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-    npairs.disable() -- Not sure if I really need this plugin.
+    -- Not sure if I really need this plugin.
+    -- npairs.disable()
 end
 
 return {
