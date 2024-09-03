a6220af4210204bd2ebfaa6800d4dfd7ce38103b
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-09-03 22:58:05 +0300

    build(telescope): up tag version

--- lua/user/plugins/nav/telescope.lua
+++ lua/user/plugins/nav/telescope.lua
@@ -260,7 +260,7 @@ end
 
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.8",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
